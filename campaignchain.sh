47226ea8223934ff2dadb8d356b4ffc41840716e
Lalit Nandan Diwakar <lalitnandandiwakar@gmail.com>
2018-04-21 23:21:26 +0530

    Update campaignchain.sh

--- campaignchain.sh
+++ campaignchain.sh
@@ -1,5 +1,4 @@
-sudo apt-get update -y
-sudo apt-get upgrade
+sudo apt-get update
 sudo git clone https://lalitnandandiwakar@bitbucket.org/lalitnandandiwakar/campaignchain.git
 sudo apt-get install php -y
 sudo apt-get install php-json -y
